4e0f469f73a33bb15ae924550568c4f8deeae232
Artem Tiuterev <tema_tuterev@mail.ru>
2021-08-11 21:28:28 +0300

    first commit2222

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl font-black">Hello World</h1>
-    </div>
+      <h1 className="text-4xl font-black">Hello World</h1>
+    </div> 
   );
 }
 
