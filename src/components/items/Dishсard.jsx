3d83ca529057aeb4f7115f727cab95a88514a110
Skmm6 <egor.qaq@gmail.com>
2022-01-09 21:25:58 +0300

    axiosadd\reduxstop4

--- src/components/items/Dishсard.jsx
+++ src/components/items/Dishсard.jsx
@@ -12,17 +12,13 @@ export default function Dishсard() {
         .then(res =>{
             console.log(res);
             setCategory(res.data)
+            setFoods(res.data.foods)
         })
         .catch(err => {
             console.log(err);
         })
     }, [])
 
-    if (category.length > 0) {
-        setFoods([foods] = category.foods)
-        console.log(foods);
-    }
-    
     
 
 
@@ -32,6 +28,7 @@ export default function Dishсard() {
 
     return (
         <div className="overflow-auto notscrollbar flex flex-wrap min-w-min h-xxxxxl mt-0 mb-0 gap-x-7 gap-y-7 items-start justify-center">
+            
                {foods.length > 0 && foods.map(food => (
                     <div key={food.id} className="inline-block min-w-192">
                     <div className=" mt-9 bg-basedark pl-7 pr-7 pb-2 pt-0 rounded-2xl flex flex-col justify-center items-center content-center overflow-visible">
